bb1a1c724db22880699ffd7460b53f74baf15d55
Mateusz Murak <mateuszmurak@yahoo.com>
2023-05-11 15:26:33 +0200

    Ponderings on version of junit to use. Collision with archaic version from book.

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,7 +1,15 @@
 package tacos;
 
+// From template
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+// From book
+/*import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.spring.boot.test.context.SpringBootTest;
+import org.spring.test.context.junit4.SpringRunner;
+*/
 
 @SpringBootTest
 class TacoCloudApplicationTests {
